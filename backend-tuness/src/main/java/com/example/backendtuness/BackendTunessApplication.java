3e93a938a12f35d60841846f515eef417056e8f6
Burak Zor <burakylmzzor@gmail.com>
2025-08-19 17:35:44 +0300

    added backend project

---
+++ backend-tuness/src/main/java/com/example/backendtuness/BackendTunessApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendtuness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTunessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTunessApplication.class, args);
+    }
+
+}
---
+++ backend-tuness/src/test/java/com/example/backendtuness/BackendTunessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendtuness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTunessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
